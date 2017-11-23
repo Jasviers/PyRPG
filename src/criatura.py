3158a7663574bff05149d296766541e58a52a679
Andrés Sánchez <andres.sanchez.marin@alumnos.upm.es>
2017-11-23 18:50:24 +0100

    Added levels system

--- src/criatura.py
+++ src/criatura.py
@@ -1,18 +1,22 @@
 class Criatura(object):
-        HP = 0
-        ATK = 0
-        DEF = 0
 
-        def __init__(self,  HP, ATK, DEF):
+        def __init__(self,  HP=100, ATK=30, DEF=10, expGiven=10):
             self.HP = HP
+            self.totalHP = HP
             self.ATK = ATK
             self.DEF = DEF
+            self.vivo = True
+            self.expGiven = expGiven
 
         def atacar(self, criatura_atacada):
                 criatura_atacada.atacado(self.ATK)
 
         def mostrarHP(self):
-                print(self.HP)
+                print(self.HP + "/" + self.totalHP)
 
         def atacado(self, ATK):
                 self.HP -= self.ATK - self.DEF
+                if(self.HP < 0):
+                        self.HP = 0
+                        self.vivo = False
+                        print("No le pegues más, que ya está muerto!")
--- src/guerrero.py
+++ src/guerrero.py
@@ -2,16 +2,32 @@ from criatura import Criatura
 
 
 class Guerrero(Criatura):
-    furia = 0
-    nombre = ""
-
     def __init__(self, HP, ATK, DEF, furia, nombre):
         Criatura.__init__(self, HP, ATK, DEF)
         self.furia = furia
+        if(furia < 0 || furia > 100):
+            raise Error("La furia debe de ser menor de 100!")
         self.nombre = nombre
+        self.nivel = 1
+        self.siguienteNivel
 
     def ataqueEspecial(self, criatura_atacada):
         if self.furia > 0:
             criatura_atacada.atacado(self.ATK*1.5)
         else:
             criatura_atacada.atacado(self.ATK)
+        if !criatura_atacada.vivo:
+            print("Enhorabuena, has matado a " + criatura_atacada.nombre)
+
+    def ganarFuria(self):
+        # Cada turno la furia aumenta según la que tengas
+        self.furia = self.furia*1.5
+
+    def atacar(self, criatura_atacada):
+        Criatura.atacar(self, criatura_atacada.vivo)
+        if(!criatura_atacada.vivo):
+            self.siguienteNivel -= criatura_atacada.expGiven
+        if(self.siguienteNivel <= 0):
+            self.nivel++
+            print("Enhorabuena! Has subido al nivel " + nivel)
+            self.siguienteNivel = nivel * 100 + self.siguienteNivel

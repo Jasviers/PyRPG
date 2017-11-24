528e44ae0c6148715e55a0775d114f1a8b73890f
Antonio Ramirez <justantorami@gmail.com>
2017-11-24 12:33:07 +0100

    WIP

--- src/juego.py
+++ src/juego.py
@@ -2,14 +2,14 @@ from monstruo import *
 from guererro import *
 from criatura import *
 
-Guerrero_Generico = Guerrero(5,3,3,50,input("¿Como te llamas?"))
+Guerrero_Generico = Guerrero(5,3,3,50,input("¿Como te llamas? "))
 Guerrero_Generico.mostrarVida()
 Monstruo_Generico = Monstruo(3,4,3,input("¿Contra que te enfrentas?"))
 Monstruo_Generico.mostrarVida()
 
 while(Guerrero_Generico.HP > 0 | Monstruo_Generico.HP > 0):
     print(Guerrero_Generico.Nombre & Guerrero_Generico.FU & Guerrero_Generico.HP)
-    print("Que haras a continuacion? 1-> Ataque Normal 2-> Ataque Especial 3-> Huir(que es lo mejor siempre)")
+    print("Que haras a continuacion? 1-> Ataque Normal 2-> Ataque Especial 3-> Huir")
     if (input() == 1):
         Guerrero_Generico.atacar(Monstruo_Generico)
         Guerrero_Generico.ganarFuria()
